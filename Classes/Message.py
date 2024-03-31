839df6e28c8b2720283b789a8b93aacc547a1c67
Cristian <cristian2937@gmail.com>
2024-03-31 19:44:37 +0200

    WIP: Aggiunto message_type a classe Message

--- Classes/Message.py
+++ Classes/Message.py
@@ -1,5 +1,6 @@
 class Message:
-    def __init__(self, user: str,text: str):
+    def __init__(self, user: str,text: str,message_type: str):
         self.user = user
         self.text = text
+        self.message_type = message_type
         
